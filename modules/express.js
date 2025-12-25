b4179a0a6adde4c3aaae9c3975151e29535fb922
Yan Ernesto <yan.ernesto@rssolutions.com.br>
2025-12-25 03:26:21 -0300

    feat: Add EJS templating for user views and implement Express routes for user management and rendering.

--- modules/express.js
+++ modules/express.js
@@ -3,11 +3,31 @@ const UserModel = require('../src/models/user.model')
 const app = express()
 app.use(express.json());
 
+app.set("view engine", "ejs")
+app.set("views", "src/views")
+
+//Isso é um middleware
+// Middleware é uma função que executa algo entre a requisição e a 
+// resposta antes de chegar no endpoint, isso intercepta qualquer requisição, 
+// se o next não for chamado ele não vai para o endpoint
+app.use((req, res, next) =>{
+  console.log("Middleware")
+  console.log(`request type: ${req.method}`)
+  console.log(`content type: ${req.headers['content-type']}`)
+
+  console.log(req.body)
+  next()
+})
 app.get('/home', (req, res) => {
   res.contentType("Application/Html");
   res.status(200).send("<h1>Ola isso aqui é com express</h1>");
 })
 
+app.get('/views/users', async (req, res) =>{
+  const users = await UserModel.find({});
+
+  res.render("index", {users})
+})
 
 app.get('/users', async (req, res) => {
   try {
@@ -57,6 +77,23 @@ app.patch("/users/:id", async (req, res) => {
     res.status(200).json(user)
 
   } catch (error) {
+    return res.status(500).send(error.message)
+  }
+})
+
+app.delete("/users/:id", async(req, res) =>{
+  try{
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndDelete(id)
+
+    if(!user){
+      return res.status(404).send("User not found")
+    }
+
+    res.status(200).json(user)
+
+  }
+  catch(error){
     return res.status(500).send(error.message)
   }
 })
